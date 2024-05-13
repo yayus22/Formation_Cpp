bdb65efc4b329441bfe057c7a814ff486f1f3584
Yaya Niare <niare.yaya@courrier.uqam.ca>
2024-05-12 22:54:06 -0400

    Ajoute l'exercice1

---
+++ Exercice1/exercice1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "exercice1.h"
+
+void testCoutEtEndl() {
+    std::cout << "Hello world" << std::endl;
+    std::cout << "Voici un programme illustrant l'utilisation de cout!" << std::endl;
+}
+
+int main(){
+    testCoutEtEndl();
+    return 0;
+}
---
+++ Exercice1/exercice1.h
@@ -0,0 +1,11 @@
+#if !defined(_EXERCICE1__H_)
+#define _EXERCICE1__H_
+
+#include <iostream>
+
+class Exercice1{
+    public :
+        void testCoutEtEndl();
+};
+
+#endif
